46a1daf2a5c804c8942ff7b3f298086d320c1242
Mathiore <mraimundo@edu.univali.br>
2021-11-21 16:07:26 -0300

    Topbar Nav and Logo

--- src/Topbar/topbar.jsx
+++ src/Topbar/topbar.jsx
@@ -1,6 +1,24 @@
 import './topbar.css'
+import logo from './img/logo.png'
 
 export default Topbar =>
-    <div>
+    <div className="topbar">
+        <div className="barWrapper">
+            <div className="topLeft">
+                <div className="logo"><img src={logo} alt="" />match it</div>
+            </div>
+            <div className="topRight">
+                <nav>
+                    <ul>
+                        <li>Início</li>
+                        <li>Criar partida</li>
+                        <li>Procurar partida</li>
+                        <li>Sign in</li>
+                        
+                    </ul>
+                </nav>
+                
+            </div>
+        </div>
         
     </div>
