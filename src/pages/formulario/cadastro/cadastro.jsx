8b3024e0cb6d15fe5d79b6911c64f1f5b2734102
Mathiore <mraimundo@edu.univali.br>
2021-11-23 19:32:07 -0300

    cadastro styling

--- src/pages/formulario/cadastro/cadastro.jsx
+++ src/pages/formulario/cadastro/cadastro.jsx
@@ -7,6 +7,20 @@ export default function Cadastro(){
                 <div>
                     <h2 className="title">Cadastro</h2>
                 </div>
+                <form className="form-wrapper">
+                    <div className="name">
+                        <label htmlFor="label">Nome Completo</label>
+                        <input type="text" className="inputCadastro" required/>
+                    </div>
+                    <div className="email">
+                        <label htmlFor="label">Email</label>
+                        <input type="email" className="inputCadastro" required/>
+                    </div>
+                    <div className="password">
+                        <label htmlFor="label">Senha</label>
+                        <input type="password" className="inputCadastro" required/>
+                    </div>
+                </form>
             </div>
         </div>
     )
