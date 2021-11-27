d50a27a67005552d651aa97991cf559b7bf3f13c
Mathiore <mraimundo@edu.univali.br>
2021-11-27 17:48:27 -0300

    Cards de procurar partida

--- src/pages/procurarPartida/procurarPartida.jsx
+++ src/pages/procurarPartida/procurarPartida.jsx
@@ -1,9 +1,50 @@
 import './procurarPartida.css'
+import {MeetingRoomOutlined} from '@material-ui/icons';
 
 export default function ProcurarPartida(){
     return(
         <div className="procurar">
-            
+            <div className="card-container">
+                <div className="pesquisa">
+
+                </div>
+                <div className="card">
+                    <div className="conteudo">
+                        <h1 className="conteudo-titulo">
+                            Partidas em Aberto
+                        </h1>
+                        <ul className="list-partidas">
+                            <li className="partidas">
+                                <span className="nome-partida">Jogo de Basquete</span>
+                                <span className="num-jogadores"> 1/6</span>
+                                <span className="entrar"><MeetingRoomOutlined/></span>
+                            </li>
+                            <li className="partidas">
+                                <span className="nome-partida">Jogo de Futebol</span>
+                                <span className="num-jogadores"> 3/6</span>
+                                <span className="entrar"><MeetingRoomOutlined/></span>
+                            </li>
+                            <li className="partidas">
+                                <span className="nome-partida">Jogo de Vôlei</span>
+                                <span className="num-jogadores"> 5/6</span>
+                                <span className="entrar"><MeetingRoomOutlined/></span>
+                            </li>
+                            <li className="partidas">
+                                <span className="nome-partida">Jogo do Joãozinho</span>
+                                <span className="num-jogadores"> 4/6</span>
+                                <span className="entrar"><MeetingRoomOutlined/></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="conteudo">
+                        <div className="conteudo-titulo">
+                            <h1>Localização</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
